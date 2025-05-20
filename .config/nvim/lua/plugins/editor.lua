533566534649f842dfb14eac8920558af9f43449
glitchdetected <braveraptor99@gmail.com>
2025-05-20 12:14:29 -0400

    add some plugins

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -14,6 +14,28 @@ return {
 	},
 
 	{
+  "karb94/neoscroll.nvim",
+  opts = {},
+},
+
+{
+    'Xuyuanp/scrollbar.nvim',
+    -- no setup required
+    init = function()
+        local group_id = vim.api.nvim_create_augroup('scrollbar_init', { clear = true })
+
+        vim.api.nvim_create_autocmd({ 'BufEnter', 'WinScrolled', 'WinResized' }, {
+            group = group_id,
+            desc = 'Show or refresh scrollbar',
+            pattern = { '*' },
+            callback = function()
+                require('scrollbar').show()
+            end,
+        })
+    end,
+},
+
+	{
 		"echasnovski/mini.hipatterns",
 		event = "BufReadPre",
 		opts = {
