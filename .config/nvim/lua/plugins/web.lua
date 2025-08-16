0d78ef3e270a32376e0ec01d443c9b086464d7f9
glitchdetected <braveraptor99@gmail.com>
2025-08-16 17:05:27 -0400

    few more things

--- .config/nvim/lua/plugins/web.lua
+++ .config/nvim/lua/plugins/web.lua
@@ -8,5 +8,25 @@ return {
       table.insert(opts.ensure_installed, "http")
     end,
   }
+},
+  {
+  "tpope/vim-fugitive",
+  cmd = {
+    "G",
+    "Git",
+    "Gdiffsplit",
+    "Gread",
+    "Gwrite",
+    "Ggrep",
+    "GMove",
+    "GDelete",
+    "GBrowse",
+  },
+  keys = {
+    { "<leader>gs", ":Git<CR>", desc = "Git status" },
+    { "<leader>gc", ":Git commit<CR>", desc = "Git commit" },
+    { "<leader>gp", ":Git push<CR>", desc = "Git push" },
+    { "<leader>gl", ":Git log<CR>", desc = "Git log" },
+  },
 }
 }
